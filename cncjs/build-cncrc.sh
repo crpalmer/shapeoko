fbd24bfa842c964d3a79e0d8a4e9494c63fe3ce0
Christopher R. Palmer <crpalmer@gmail.com>
2022-11-12 14:10:11 -0500

    cncjs: setup gsender macros

--- cncjs/build-cncrc.sh
+++ cncjs/build-cncrc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$1" == "--cncrc" ]; then
+    cncrc=1
+    shift
+else
+    cncrc=0
+fi
+
+if [ "$cncrc" == 1 ]; then
 cat <<EOF
 {
     "state": {
@@ -11,23 +19,31 @@ cat <<EOF
         }
     },
     "secret": "\$2a\$10\$wzsxrunfn5euI2J6NqzxwO",
-    "macros": [
+    "macros": 
 EOF
+fi
 
+echo "["
 bracket=""
 for i in $*
 do
     echo >&2 "$i..."
     if [ -n "$bracket" ]; then echo "$bracket"; fi
     echo "        {"
-    echo "            \"id\": \"`uuidgen`\","
-    echo "            \"mtime\": \"`stat --format=%Y $i`000\","
+    if [ "$cncrc" == 1 ]; then
+        echo "            \"id\": \"`uuidgen`\","
+        echo "            \"mtime\": \"`stat --format=%Y $i`000\","
+    fi
     echo "            \"name\": \"`echo $i | sed 's/_/ /g' | sed 's/.txt$//'`\","
     echo "            \"content\": `jq -Rsa < $i`"
     bracket="        },"
 done
+
 cat <<EOF 
         }
     ]
-}
 EOF
+
+if [ "$cncrc" == 1 ]; then
+    echo "}"
+fi
