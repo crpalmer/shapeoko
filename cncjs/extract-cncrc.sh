bf472f7139261991cdb70a321f116c661b105845
Christopher R. Palmer <crpalmer@gmail.com>
2022-11-19 07:23:53 -0500

    Automatically strip blank lines at end of macros

--- cncjs/extract-cncrc.sh
+++ cncjs/extract-cncrc.sh
@@ -5,5 +5,7 @@ do
     read -r content
     name=`echo "$name" | sed 's/"name": "//' | sed 's/", *$//' | sed 's/ /_/g'`".txt"
     echo $name
-    ( echo '{'; cat <<<$content; echo '}') | jq -r '.content' > $name
+    ( echo '{'; cat <<<$content; echo '}') | jq -r '.content' | \
+	sed -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
+	> $name
 done
