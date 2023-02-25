11fbc48a44044d87ebcbc1ce66b7878af0414e2d
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-25 14:05:49 -0500

    extract-cncrc.sh: Tidy this up to make it easier to read

--- cncjs/extract-cncrc.sh
+++ cncjs/extract-cncrc.sh
@@ -5,7 +5,11 @@ do
     read -r content
     name=`echo "$name" | sed 's/"name": "//' | sed 's/", *$//' | sed 's/ /_/g'`".txt"
     echo $name
-    ( echo '{'; cat <<<$content; echo '}') | jq -r '.content' | \
-	sed -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
-	> $name
+    (
+	echo '{'
+	cat <<<$content
+	echo '}'
+    ) | jq -r '.content' \
+      | sed -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
+      > $name
 done
